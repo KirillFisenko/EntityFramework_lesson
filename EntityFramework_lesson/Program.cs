47b76b2e32a5ab400cbe959c355ad7d53b2e29a3
KirillFisenko <79265463469@ya.ru>
2025-02-13 19:47:33 +0300

    4.3 Контекст данных и модели

--- EntityFramework_lesson/Program.cs
+++ EntityFramework_lesson/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+
+public class Phone
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Phone> Phones { get; set; }
+
+    public ApplicationDbContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=devices;Uid=root;Pwd=m48kHz16bit%;";
+        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var phone = new Phone()
+        {
+            Name = "Xiaomi Poco F4",
+            Price = 25000
+        };
+
+        using var dbContext = new ApplicationDbContext();
+        dbContext.Phones.Add(phone);
+        dbContext.SaveChanges();
+    }
+}
