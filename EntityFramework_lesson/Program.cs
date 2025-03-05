4399baf38b75fc6dbe6514ad983a1d500208d467
KirillFisenko <79265463469@ya.ru>
2025-03-05 15:01:51 +0300

    +

--- EntityFramework_lesson/Program.cs
+++ EntityFramework_lesson/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 public class Phone
 {
@@ -7,11 +8,11 @@ public class Phone
     public decimal Price { get; set; }
 }
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext1 : DbContext
 {
     public DbSet<Phone> Phones { get; set; }
 
-    public ApplicationDbContext()
+    public ApplicationDbContext1()
     {
         Database.EnsureDeleted();
         Database.EnsureCreated();
@@ -24,24 +25,44 @@ public class ApplicationDbContext : DbContext
     }
 }
 
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Phone> Phones { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+}
+
 public class Program
 {
-    public static void Main1()
+    public static void Main()
     {
+        var builder = new ConfigurationBuilder();
+        builder.SetBasePath(Directory.GetCurrentDirectory());
+        builder.AddJsonFile("appsettings.json");
+        var config = builder.Build();
+
+        var connectionString = config.GetConnectionString("DefaultConnection");
+
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        var options = optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)).Options;
+
         var phone = new Phone()
         {
             Name = "Xiaomi Poco F4",
             Price = 25000
         };
 
-        using var dbContext = new ApplicationDbContext();
+        using var dbContext = new ApplicationDbContext(options);
         dbContext.Phones.Add(phone);
         dbContext.SaveChanges();
     }
 
-    public static void Main()
+    public static void Main1()
     {
-        using var dbContext = new ApplicationDbContext();
+        using var dbContext = new ApplicationDbContext1();
 
         foreach (var phone in dbContext.Phones)
         {
